e4a785e40b19cc747b1e37719f29d4c3f70299db
Russell Spence <rmspence@wisc.edu>
2022-03-03 13:39:49 +0000

    Added naviagation to a blank weather page and got rid of some useless styling in weather and addclothes

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,7 +1,8 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from 'react-navigation';
-import AddClothes from '../screens/addClothes'
+import AddClothes from '../screens/addClothes';
 import Recommended from "../screens/recommended";
+import Weather from '../screens/weather.js';
 
 const screens = {
     Home: {
@@ -12,8 +13,11 @@ const screens = {
     },
     AddClothes: {
         screen: AddClothes
-    }
-
+    },
+    Weather: {
+        screen: Weather
+    },
+// ADD TASK TO TRELLO PAGE
 }
 
 const homeStack = createStackNavigator(screens, {
