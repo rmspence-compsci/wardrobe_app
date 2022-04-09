a1ae73275e6c1658a1130f7fd2c5e289db159afd
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2022-04-09 21:03:18 +0100

    Add some utility functions useful for other factors

--- recommender.js
+++ recommender.js
@@ -1,4 +1,39 @@
 /**
+ * Create a numerical filter function for a key within an object
+ * @param {string} key The key to filter by
+ * @param {number} [min] The minimum numerical value an object can have
+ * @param {number} [max] The maximum numerical value an object can have
+ * @returns The filter function
+ */
+function filterByNumericalProperty(key, min, max) {
+  if (min && max) return function(obj) { return obj[key] > min && obj[key] < max }
+  else if (min) return function(obj) { return obj[key] > min }
+  else if (max) return function(obj) { return obj[key] < max }
+  else return function(obj) { return true }
+}
+
+/**
+ * Create a generic filter function for a key within an object
+ * @param {string} key The key to filter by
+ * @param {*} value The value to check for
+ * @returns The filter function
+ */
+function filterByPropertyValue(key, value) {
+  return function(obj) { return obj[key] == value }
+}
+
+/**
+ * Create a numerical sort function for a key within an object
+ * @param {string} key The key to sort by
+ * @param {boolean} [desc] Whether to create a descending sort function, defaults to true
+ * @returns The sort function
+ */
+function sortByNumericalProperty(key, desc = true) {
+  if (desc) return function(a, b) { return b[key] - a[key]; }
+  else return function(a, b) { return a[key] - b[key]; }
+}
+
+/**
  * A clothing type.
  * @enum {string}
  */
