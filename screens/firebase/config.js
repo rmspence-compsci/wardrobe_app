f96dc907b6b4526ff0f8477eed5129a50c2d1a5a
Seandowling123 <61026772+Seandowling123@users.noreply.github.com>
2022-04-13 16:15:14 +0100

    firebase config for add_events page

---
+++ screens/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqV16ERivqLQohX97F80DvRl-iCHojv04",
+    authDomain: "events-manager-5c2f7.firebaseapp.com",
+    projectId: "events-manager-5c2f7",
+    storageBucket: "events-manager-5c2f7.appspot.com",
+    messagingSenderId: "613665945763",
+    appId: "1:613665945763:web:2e5b53266aee89865ca716",
+    measurementId: "G-F5H2200KVM"
+  };
+  
+  // Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
+
+ export const db = getFirestore(app);
